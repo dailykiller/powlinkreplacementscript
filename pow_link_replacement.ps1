eaa8d97558a814875a49bfe0f548421b36ae9918
dailykiller <129468694+dailykiller@users.noreply.github.com>
2023-04-01 00:03:55 -0400

    updated script

--- pow_link_replacement.ps1
+++ pow_link_replacement.ps1
@@ -3,15 +3,11 @@ $computers = Get-Content .\computers.txt
 #deletes all versions of POW from computers
 foreach ($computer in $computers) {
     
-    
-    $pow_public_items = Get-ChildItem "\\$computer\C$\Users\Public\Desktop" -Filter "POW*" -Recurse | ForEach-Object { $_.FullName }
-    Remove-Item $pow_public_items -Recurse -ErrorAction Ignore
-    
-    
-    $pow_default_items = Get-ChildItem "\\$computer\C$\Users\Default\Desktop" -Filter POW* -Recurse | ForEach-Object { $_.FullName }
-    Remove-Item $pow_default_items -Recurse -ErrorAction Ignore
 
+    $pow_users_items = Get-ChildItem "\\$computer\C$\Users" -Filter POW* -Recurse | ForEach-Object {$_.Fullname}
+    Remove-Item $pow_users_items -Recurse -ErrorAction Ignore
 
+    
 }
 
 #Adds shortcuts and local files for POW icon to computers exports failure log if fails
